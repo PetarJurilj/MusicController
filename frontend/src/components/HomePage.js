f7185cabbe30077a1b17f8de2911412acd34c166
Petar <petarjurilj93@gmail.com>
2021-07-21 20:46:44 +0200

    initial commit

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from 'react-router-dom'
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Switch>
+                        <Route exact path="/">
+                            <p>this is home page</p>
+                        </Route>
+                        <Route path="/join" component={RoomJoinPage}/>
+                        <Route path="/create" component={CreateRoomPage}/>
+                        <Route path="/room/:roomCode" component={Room} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
